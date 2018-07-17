0e1b9ff630a3d496aa37ceda030ef66509088ae8
Jonathan Castro dos Santos <jonathandossantos.c@gmail.com>
2018-07-17 16:27:27 -0300

    chore: Add Reac Router Dependencies

--- src/SearchView.js
+++ src/SearchView.js
@@ -1,23 +1,74 @@
-import React, {Component} from 'react';
-import './App.css'
-import Book from './Book';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import Book from './Book'
+import * as BooksAPI from './BooksAPI'
 
 class SearchView extends Component {
-
-    render() {
-     return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.shelfName}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-        {this.props.currentShelf.map((book) => (
-         <Book key={book.id} book={book} updateShelf={this.props.updateShelf} />
-        ))}
-        </ol>
-      </div>
-      </div>
-     )
+    state = {
+        query: '',
+        books: [],
+        filteredBooks: []
     }
-  }
 
-  export default SearchView
+    updateQuery = (query) => {
+        this.setState({query})
+            if(query){
+                BooksAPI.search(query).then((books) => { 
+                    if(books instanceof Array)  {
+                        //add books to state
+                        this.setState({books})
+                    }
+                    else {
+                        //set book state to empty array
+                        this.setState({books: []})
+                    }
+
+                }
+            )
+        }    
+    }
+    
+    render() {
+        
+        const {onChangeCategory} = this.props
+        const {query, books} = this.state
+        
+        return (
+            <div>
+            <div className="search-books-bar">
+                <Link className="close-search" to="/">Close</Link>
+                <form>
+                    <div className="search-books-input-wrapper">
+                        <input type='text' placeholder='Search books by title or author' value={query} onChange={(event) => this.updateQuery(event.target.value)} />
+                    </div>
+                </form>
+            </div>
+           {books.length!==0 && (
+                <div className="search-books-results">
+                    <div className="search-books">
+                        <ol className="books-grid">
+                            {books.map((book) => (  
+                                <li key={book.id}>
+                                    <Book
+                                        onChangeCategory={onChangeCategory}
+                                        book = {book}
+                                    />
+                                </li>
+                            ))}
+                        </ol>
+                    </div>
+                </div>
+               )}
+               {(books.length===0 && query.length!==0) && (
+               <div className="search-results">
+                    {`No book found`}
+                </div>
+               )}
+            </div> 
+
+        )
+    }
+    
+}
+
+export default SearchView
