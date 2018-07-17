1a64e2fd694213b34de147e5ae87b97747207209
Jonathan Castro dos Santos <jonathandossantos.c@gmail.com>
2018-07-17 16:34:57 -0300

    refactor: Transform Book in a Component

--- src/Book.js
+++ src/Book.js
@@ -1,28 +1,31 @@
 import React, {Component} from 'react';
-import './App.css'
 
 class Book extends Component {
 
     render() {
+        
+        const {book, onChangeCategory} = this.props        
+        
         return (
-            <li className="book-list-item" key={this.props.book.id}>
+            <div>
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`}}></div>
-                        <div className="book-shelf-changer">
-                            <select value={this.props.book.shelf} id="select-shelf" onChange={(event) => {this.props.updateShelf(this.props.book, event)}}>
-                                <option disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                            </select>
-                        </div>
-                    </div>
-                <div className="book-title">{this.props.book.title}</div>
-                <div className="book-authors">{this.props.book.authors}</div>
+                {book.imageLinks && <div className="book-cover" style={{width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`}}></div>}
+                <div className="book-shelf-changer">
+                    <select tabIndex="0" ref={book.shelf} aria-label="Choose category for book" className="select-shelf" value={book.shelf} onChange={(event) => onChangeCategory(book, event.target.value)}>>        
+                        <option value="test">Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                    </select>
+                </div>
             </div>
-            </li>
+          {book.title && <div className="book-title" tabIndex="0">{book.title}</div>}
+          {book.authors && <div className="book-authors" tabIndex="0">{book.authors}</div>}
+        </div>
+        
+        </div>
         )
     }
 }
