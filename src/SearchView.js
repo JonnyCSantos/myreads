4a5cb817ef2cb86e2d73a1cae9f24928f0aeb766
Jonathan Castro dos Santos <jonathandossantos.c@gmail.com>
2018-07-20 14:21:01 -0300

    refactor: change the way I bring books in search

--- src/SearchView.js
+++ src/SearchView.js
@@ -2,30 +2,35 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import Book from './Book'
 import * as BooksAPI from './BooksAPI'
+import BooksApp from './App';
 
 class SearchView extends Component {
     state = {
         query: '',
-        books: [],
         filteredBooks: []
     }
 
     updateQuery = (query) => {
         this.setState({query})
             if(query){
-                BooksAPI.search(query).then((books) => { 
-                    if(books instanceof Array)  {
-                        //add books to state
-                        this.setState({books})
-                    }
-                    else {
-                        //set book state to empty array
-                        this.setState({books: []})
-                    }
-
+                BooksAPI.search(query)
+                    .then((books) => { 
+                        if(books == 'error') {
+                            console.log('Erro!');
+                            this.setState({filteredBooks: []})
+                        } else {
+                            this.state.filteredBooks = books.filter(function (b) {
+                                if(b === this.props.books){
+                                    b = BooksApp.shelf
+                                } else {
+                                    b.shelf = 'none'
+                                }
+                                return this.setState(this.state.filteredBooks)
+                            })
+                        }
                 }
             )
-        }    
+        }
     }
     
     render() {
@@ -39,7 +44,7 @@ class SearchView extends Component {
                 <Link className="close-search" to="/">Close</Link>
                 <form>
                     <div className="search-books-input-wrapper">
-                        <input type='text' placeholder='Search books by title or author' value={query} onChange={(event) => this.updateQuery(event.target.value)} />
+                        <input type='text' placeholder='Search books by title or author' value={query} onChange={(event) => this.updateQuery(event)} />
                     </div>
                 </form>
             </div>
@@ -58,17 +63,15 @@ class SearchView extends Component {
                         </ol>
                     </div>
                 </div>
-               )}
-               {(books.length===0 && query.length!==0) && (
-               <div className="search-results">
-                    {`No book found`}
-                </div>
-               )}
+            )}
+            {(books.length===0 && query.length!==0) && (
+            <div className="search-results">
+                 {`No book found`}
+             </div>
+            )}
             </div> 
-
         )
     }
-    
 }
 
 export default SearchView
